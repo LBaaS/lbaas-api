90776fe41debb1f4575b5b840ad519c8b8d7b19e
Peter Mellquist <pemellquist@gmail.com>
2012-11-03 03:24:17 +0000

    refactor for all db access

--- src/main/java/org/pem/lbaas/persistency/DeviceDataModel.java
+++ src/main/java/org/pem/lbaas/persistency/DeviceDataModel.java
@@ -1,7 +1,8 @@
 package org.pem.lbaas.persistency;
 
 /**
- * pemellquist@gmail.com
+ * Persistency class for LBaaS Devices
+ * @author Peter Mellquist pemellquist@gmail.com
  */
 
 import java.text.SimpleDateFormat;
@@ -18,7 +19,24 @@ import org.pem.lbaas.datamodel.LoadBalancer;
 public class DeviceDataModel {
    private static Logger logger       = Logger.getLogger(DeviceDataModel.class);
    protected Connection  dbConnection = null;
+   
+   public final static String SQL_ID            = "id";
+   public final static String SQL_NAME          = "name";
+   public final static String SQL_ADDRESS       = "address";
+   public final static String SQL_LOADBALANCER  = "loadbalancer";
+   public final static String SQL_TYPE          = "type";
+   public final static String SQL_STATUS        = "status";
+   public final static String SQL_CREATED       = "created";
+   public final static String SQL_UPDATED       = "updated";
 	
+   /**
+    * Get a connection to database
+    * 
+    * Obtain a connection to database which tries to maintain a single connection and only re-open when needed.
+    * 
+    * @return Connection
+    * @throws DeviceModelAccessException
+    */
    protected Connection dbConnect() throws DeviceModelAccessException {
       if ( dbConnection==null ) {
 	     try {           
@@ -52,9 +70,10 @@ public class DeviceDataModel {
          }
       }
    }
-   
-   
-   
+      
+   /**
+    * Close the database connection
+    */
    protected void dbClose() {
       if (dbConnection != null)
       {
@@ -69,22 +88,31 @@ public class DeviceDataModel {
    }
 	
    
+   /**
+    * finalize'r closes database 
+    */
    protected void finalize ()  {
       dbClose();
    }
 
-   
+   /**
+    * Convert SQL result set to a Device object
+    * 
+    * @param rs
+    * @return Device
+    * @throws SQLException
+    */
    protected Device rsToDevice(ResultSet rs) throws SQLException {
       Device device = new Device();
       try {
-         device.setId(new Integer(rs.getInt("id")));
-         device.setName(rs.getString("name"));
-         device.setAddress(rs.getString("address"));
-         device.setLbId(new Integer (rs.getInt("loadbalancer")));
-         device.setLbType(rs.getString("type"));
-         device.setStatus(rs.getString("status"));	
-         device.setCreated(rs.getString("created"));
-         device.setUpdated(rs.getString("updated"));
+         device.setId(new Integer(rs.getInt(SQL_ID)));
+         device.setName(rs.getString(SQL_NAME));
+         device.setAddress(rs.getString(SQL_ADDRESS));
+         device.setLbId(new Integer (rs.getInt(SQL_LOADBALANCER)));
+         device.setLbType(rs.getString(SQL_TYPE));
+         device.setStatus(rs.getString(SQL_STATUS));	
+         device.setCreated(rs.getString(SQL_CREATED));
+         device.setUpdated(rs.getString(SQL_UPDATED));
 	  }
 	  catch (SQLException sqle) {                                              
          logger.error( "SQL Exception : " + sqle); 
@@ -94,7 +122,12 @@ public class DeviceDataModel {
 	}
 	
 	
-   
+   /**
+    * get all devices
+    * 
+    * @return List of Device objects
+    * @throws DeviceModelAccessException if internal database error
+    */
    public  List<Device> getDevices() throws DeviceModelAccessException {
       List<Device> devices = new  ArrayList<Device>();		
 	  Connection conn = dbConnect();
@@ -117,6 +150,13 @@ public class DeviceDataModel {
    }
 	
 	
+   /**
+    * Get a Device based on its id
+    * 
+    * @param id
+    * @return Device or null if not found
+    * @throws DeviceModelAccessException if internal database error
+    */
    public Device getDevice(Integer id) throws DeviceModelAccessException {
       Connection conn = dbConnect();
       Statement stmt=null;	
@@ -142,6 +182,14 @@ public class DeviceDataModel {
 	}
 	
 	
+   /**
+    * Find a free Device 
+    * 
+    * Finds a free device to be use when building a loadbalancer OR null if none found
+    * 
+    * @return Device or null if none found
+    * @throws DeviceModelAccessException if internal database error
+    */
    public Device findFreeDevice() throws DeviceModelAccessException {		
       Connection conn = dbConnect();
       Statement stmt=null;		
@@ -166,7 +214,16 @@ public class DeviceDataModel {
       }		
    }
 	
-	
+
+   /**
+    * Determine if a Device name already exists
+    * 
+    * Devices names must be unique, this method checks to see if name already exists
+    * 
+    * @param name
+    * @return boolean
+    * @throws DeviceModelAccessException if internal database error
+    */
    public boolean existsName(String name) throws DeviceModelAccessException {
       Connection conn = dbConnect();
       Statement stmt=null;
@@ -185,6 +242,15 @@ public class DeviceDataModel {
    }
 	
 	
+   /**
+    * Delete a device based on its id
+    * 
+    * Deletes device and returns delete count
+    * 
+    * @param id
+    * @return 1 if deleted or 0 if not ( delete count )
+    * @throws DeviceModelAccessException if internal database error
+    */
    public int deleteDevice(Integer id) throws DeviceModelAccessException {
       Connection conn = dbConnect();
       Statement stmt=null;		
@@ -202,6 +268,16 @@ public class DeviceDataModel {
    }
 	
    
+   /**
+    * Change the status on a Device
+    * 
+    * Changes device status returns true if changed or false if Device not found
+    * 
+    * @param status
+    * @param id
+    * @return boolean
+    * @throws DeviceModelAccessException if internal database error
+    */
    public boolean setStatus(String status, Integer id) throws DeviceModelAccessException {	
       Device device = this.getDevice(id);
       if ( device == null)
@@ -211,30 +287,50 @@ public class DeviceDataModel {
       return true;	
     }
    
-	
+
+   /**
+    * Change a Device based on its ID
+    * 
+    * Change a Device based on its id, only allows change of name, address, loadbalancer and status
+    * 
+    * @param device
+    * @return boolean
+    * @throws DeviceModelAccessException if internal database error
+    */
    public boolean setDevice(Device device) throws DeviceModelAccessException {
 	   
 	  if ( this.getDevice(device.getId())==null)
 		  return false;                            // id not found
 	  
       Connection conn = dbConnect();
-      Statement stmt=null;
-      int id = device.getId().intValue();
-      String name = device.getName();
-      String address = device.getAddress();
-      int lbid = device.getLbId().intValue();		
-      String update = "UPDATE devices SET name = '" + name + "' , address = '" + address + "' , loadbalancer = " + lbid + " WHERE id = " + id;
       try {
-         stmt=conn.createStatement();
-         stmt.execute(update);		      
-         return true;
+			String query = "UPDATE devices SET name = ?, address = ?, loadbalancer = ?, status = ? WHERE id = ?";
+			PreparedStatement statement = conn.prepareStatement(query);
+			statement.setString(1, device.getName());
+			statement.setString(2, device.getAddress());
+			statement.setInt(3, device.getLbId().intValue());
+			statement.setString(4, device.getStatus());
+			statement.setInt(5,device.getId().intValue());
+			statement.executeUpdate();
+			statement.close();
+			return true;
       }
-      catch (SQLException s) {                                              
-         throw new DeviceModelAccessException("SQL Exception : " + s);             
-      }
+      catch (SQLException s) {
+	    	throw new DeviceModelAccessException("SQL Exception : " + s);   
+	  }
+			
    }
    
 	
+   /**
+    * Mark a Device as no longer being used by a Loadbalancer
+    * 
+    * sets the loadbalancer reference value to zero meaning no longer in use.
+    * 
+    * @param id
+    * @return boolean
+    * @throws DeviceModelAccessException if internal database error
+    */
    public boolean markAsFree(int id) throws DeviceModelAccessException {
       Device device = this.getDevice(id);
       if (device==null)
@@ -244,12 +340,24 @@ public class DeviceDataModel {
       return true;
    }
 	
+   /**
+    * Get summary of device usage information
+    * 
+    * @return DeviceUsage
+    */
    public DeviceUsage getUsage() {
       DeviceUsage usage = new DeviceUsage();
       return usage;
    }
 	
 
+   /**
+    * Create a new device
+    * 
+    * @param device
+    * @return new device id
+    * @throws DeviceModelAccessException if internal database error
+    */
    public Integer createDevice(Device device) throws DeviceModelAccessException {		
 		
 		int val=0;				
