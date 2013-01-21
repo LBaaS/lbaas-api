f4dca8e1fe285c4a3c78a4b22b3eb2c1609b12ec
Peter Mellquist <pemellquist@gmail.com>
2013-01-21 18:10:52 +0000

    roll to 1.5

--- lbaas.sh
+++ lbaas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JAR="/opt/lbaasapi/lbaasapi-0.1.0-jar-with-dependencies.jar"
+JAR="/opt/lbaasapi/lbaasapi-0.1.5-jar-with-dependencies.jar"
 LOGCFG="file:/opt/lbaasapi/log4j.properties"
 CONFIG="./lbaas.config"
 
