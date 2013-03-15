50dbfec9614d980cd927bbea5962b861f65011dc
Peter Mellquist <pemellquist@gmail.com>
2013-03-15 18:36:05 +0000

    roll to 1.12

--- lbaas.sh
+++ lbaas.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-JAR="/opt/lbaasapi/lbaasapi-0.1.9-jar-with-dependencies.jar"
+JAR="/opt/lbaasapi/lbaasapi-0.1.12-jar-with-dependencies.jar"
 KILLPATTERN="/opt/lbaasapi/lbaasapi-"
 LOGCFG="file:/opt/lbaasapi/log4j.properties"
 CONFIG="./lbaas.config"
